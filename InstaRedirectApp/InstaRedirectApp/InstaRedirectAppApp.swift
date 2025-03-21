9e0bb911b740188305194833203f6570461c2909
herste <hersteo@outlook.com>
2025-03-21 18:56:50 -0400

    Initial commit

---
+++ InstaRedirectApp/InstaRedirectApp/InstaRedirectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaRedirectAppApp.swift
+//  InstaRedirectApp
+//
+//  Created by EJ Emesia on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstaRedirectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaRedirectApp/InstaRedirectAppTests/InstaRedirectAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaRedirectAppTests.swift
+//  InstaRedirectAppTests
+//
+//  Created by EJ Emesia on 21/03/2025.
+//
+
+import Testing
+@testable import InstaRedirectApp
+
+struct InstaRedirectAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
